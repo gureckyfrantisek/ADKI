45dfc971812f463ede83f0ad1dab05c37fd4d3a2
Armen-cz <adam.kralic3@gmail.com>
2026-02-27 16:54:56 +0100

    Zoom and Pan options
    Added Zoom option by scrolling the wheel and Pan option by pressing the Arrow keys with implementation for drawing and adding points/polygons

--- draw.py
+++ draw.py
@@ -8,16 +8,64 @@ class Draw(QWidget):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.setFocusPolicy(Qt.FocusPolicy.StrongFocus) #Enables key inputs
         self.__pol = [Polygon()]    #An array of all polygons on screen with their id
         self.__q = QPointF(-100, -100)
         self.__add_vertex = True
         self.__algo = Algorithms()
+        self.__zoom = 1
+        self.__zoom_change = 0.9
+        self.__pan = [0, 0]
+        self.__pan_change = 40
        
+    def wheelEvent(self, event):
+        #Handles mouse wheel inputs
+        delta = event.angleDelta().y()
+
+        if delta > 0:
+            #Zoom in
+            self.__zoom /= self.__zoom_change
+        else:
+            #Zoom out
+            self.__zoom *= self.__zoom_change
+            
+        print(self.__zoom)
+        self.repaint()
+        event.accept()  #Mark event as handled
+        
+    def keyPressEvent(self, event: QKeyEvent):
+        #Handles key inputs
+        
+        #Section for moving the canvas (PAN)
+        if event.key() == Qt.Key.Key_Up:
+            self.__pan[1] += self.__pan_change / self.__zoom
+
+        elif event.key() == Qt.Key.Key_Down:
+            self.__pan[1] -= self.__pan_change / self.__zoom
+
+        elif event.key() == Qt.Key.Key_Left:
+            self.__pan[0] += self.__pan_change / self.__zoom
+
+        elif event.key() == Qt.Key.Key_Right:
+            self.__pan[0] -= self.__pan_change / self.__zoom
+
+        print(self.__pan)
+        self.repaint()
         
     def mousePressEvent(self, e):
         #Get position
-        x = e.position().x()
-        y = e.position().y()
+        
+        #Only continues with left click
+        if e.button() != Qt.MouseButton.LeftButton:
+            return
+
+        #Widget mouse position
+        x_widget = e.position().x()
+        y_widget = e.position().y()
+        
+        #Transformed coordinates
+        x = x_widget/self.__zoom - self.__pan[0]
+        y = y_widget/self.__zoom - self.__pan[1]
         
         #Add new vertex to polygon
         if self.__add_vertex:
@@ -35,6 +83,18 @@ class Draw(QWidget):
         #Repaint screen
         self.repaint()
 
+    def recalculateFeatures(self, zoom_change):
+        # Momentálně se nepoužívá, mění přímo souřadnice bodů, což asi nechceme
+        self.__q = QPointF(self.__q.x()*zoom_change, self.__q.y()*zoom_change)
+        
+        new_polygons = []
+        for pol in self.__pol:
+            scaled = Polygon()
+            for point in pol:
+                scaled.append(QPointF(point.x()*zoom_change, point.y()*zoom_change))
+            new_polygons.append(scaled)
+        
+        self.__pol = new_polygons
 
     def paintEvent(self, e):
         #Repaint screen
@@ -49,9 +109,14 @@ class Draw(QWidget):
         qp.setPen(Qt.GlobalColor.red)
         qp.setBrush(Qt.GlobalColor.yellow)
         
+        #Transform definition for polygons
+        transform = QTransform()
+        transform.scale(self.__zoom, self.__zoom)
+        transform.translate(self.__pan[0], self.__pan[1])
+        
         #Draw all polygons
         for poly in self.__pol:
-            qp.drawPolygon(poly)
+            qp.drawPolygon(transform.map(poly))
         
         #Graphic attributes, point
         qp.setPen(Qt.GlobalColor.black)
@@ -59,19 +124,21 @@ class Draw(QWidget):
         
         #Point radius
         r = 5
-        qp.drawEllipse(int(self.__q.x() -r), int(self.__q.y() - r), 2*r, 2*r )
+        transformed_x = (self.__q.x() + self.__pan[0])*self.__zoom
+        transformed_y = (self.__q.y() + self.__pan[1])*self.__zoom
+        qp.drawEllipse(int(transformed_x - r) , int(transformed_y - r) , 2*r, 2*r )
         
         #End draw
         qp.end()
         
         
     def changeStatus(self):
-        #Change status: draw point / polygon
+        """ Changes status: draw point / polygon """
         self.__add_vertex = not(self.__add_vertex)
         
         
     def clearSelection(self):
-        #Clears entire canvas
+        """ Clears entire canvas """
         self.__q = QPointF(-100, -100)
         self.__pol[0].clear()
         
