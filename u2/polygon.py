aee174581423d5813e4f3140004e322bb199eb04
Armen-cz <adam.kralic3@gmail.com>
2026-04-05 17:50:15 +0200

    Polygon class modification
    Attribut for storing Minimal Bounding Rectaqngle + information about correct/wrong classification

--- u2/polygon.py
+++ u2/polygon.py
@@ -2,10 +2,12 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 
 class Polygon(QPolygonF):
-    def __init__(self, bbox=None, id=0, *args, **kwargs):
+    def __init__(self, bbox=None, id=0, simplified_pol=None, classified=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.id = id
         self.bbox = bbox
+        self.simplified_pol = simplified_pol
+        self.classified = classified
 
     def addVertex(self, vertex):
         self.append(vertex)
