d573fbfd7c2d951fd94472e950fcd2fa1a412013
František Gurecký <gurecfra@cvut.cz>
2026-06-09 18:32:26 +0200

    feat: SHP loading
    Implemented the same way as in u2, needed

--- u4/draw.py
+++ u4/draw.py
@@ -1,14 +1,15 @@
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
+import shapefile as shp
 
 class Draw(QWidget):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__polyline = []
+        self.__polylines = [[]]
         #self.setFocusPolicy(Qt.FocusPolicy.StrongFocus) #Enables key inputs
-        self.__polyline_simp = []
+        self.__polylines_simp = []
         #self.__polyline = self.createTestPoints()
         self.__zoom = 1
         self.__zoom_change = 0.75
@@ -73,7 +74,7 @@ class Draw(QWidget):
         p = QPointF(x,y)
             
         #Add point to polyline
-        self.__polyline.append(p)      
+        self.__polylines[-1].append(p)
       
         #Repaint screen
         self.update()
@@ -108,49 +109,50 @@ class Draw(QWidget):
 
         qp.setPen(Qt.GlobalColor.black)
 
-        if self.__polyline:
-            #Draw polyline
-            poly = QPolygonF(self.__polyline)
-            qp.drawPolyline(transform.map(poly))
-            #Graphic attributes, simplified polyline
-            
-            #Draw simplified polyline
-            qp.setPen(Qt.GlobalColor.red)
-            poly_simp = QPolygonF(self.__polyline_simp)
-            qp.drawPolyline(transform.map(poly_simp))
-            
+        #Draw polylines
+        qp.setPen(Qt.GlobalColor.black)
+        for pl in self.__polylines:
+            if len(pl) >= 2:
+                qp.drawPolyline(transform.map(QPolygonF(pl)))
+
+        #Draw simplified polylines
+        qp.setPen(Qt.GlobalColor.red)
+        for pl in self.__polylines_simp:
+            if len(pl) >= 2:
+                qp.drawPolyline(transform.map(QPolygonF(pl)))
+
         qp.end()
         
         
     def clearCanvas(self):
         #Clears the canvas
-        self.__polyline.clear()
-        self.__polyline_simp.clear()
+        self.__polylines = [[]]
+        self.__polylines_simp = []
 
         self.repaint()
 
 
     def clearResult(self):
         #Clear results of analyses
-        self.__polyline_simp.clear()
-           
+        self.__polylines_simp = []
+
         #Repaint screen
         self.repaint()
 
     
-    def getPolyline(self):
-        #Get polyline
-        return self.__polyline
-    
+    def getPolylines(self):
+        #Get polylines
+        return [pl for pl in self.__polylines if len(pl) >= 2]
 
-    def setPolylineSimp(self, polyline_simp):
-        #Set simplified polyline
-        self.__polyline_simp = polyline_simp
 
-    
-    def getPolylineSimp(self):
-        #Set simplified polyline
-        return self.__polyline_simp
+    def setPolylinesSimp(self, polylines_simp):
+        #Set simplified polylines
+        self.__polylines_simp = polylines_simp
+
+
+    def getPolylinesSimp(self):
+        #Get simplified polylines
+        return self.__polylines_simp
     
 
     def createTestPoints(self):
@@ -162,4 +164,51 @@ class Draw(QWidget):
             points.append(QPointF(200 + p[0]*100, 200 + p[1]*100))
 
         return QPolygonF(points)
-     
+
+
+    def saveSHPData(self, sf):
+        self.__polylines = []
+        for shape in sf.shapes():
+            part_ends = list(shape.parts[1:]) + [len(shape.points)]
+            for start, end in zip(shape.parts, part_ends):
+                pts = [QPointF(x, -y) for x, y in shape.points[start:end]]
+                if len(pts) >= 2:
+                    self.__polylines.append(pts)
+
+
+    def getFile(self):
+        file, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Shapefiles (*.shp);;All Files (*)")
+        return file
+
+
+    def handleFileOpen(self):
+        file = self.getFile()
+        if not file:
+            return
+        sf = shp.Reader(file)
+        self.saveSHPData(sf)
+        self.__polylines_simp = []
+        self.zoomToData()
+
+
+    def zoomToData(self):
+        all_pts = [p for pl in self.__polylines for p in pl]
+        if not all_pts or self.width() == 0 or self.height() == 0:
+            return
+
+        x_min = min(p.x() for p in all_pts)
+        x_max = max(p.x() for p in all_pts)
+        y_min = min(p.y() for p in all_pts)
+        y_max = max(p.y() for p in all_pts)
+
+        w = x_max - x_min
+        h = y_max - y_min
+        if w == 0 or h == 0:
+            return
+
+        padding = 0.9
+        self.__zoom = min((self.width() * padding) / w, (self.height() * padding) / h)
+        self.__pan[0] = (self.width() / (2 * self.__zoom)) - (x_min + w / 2)
+        self.__pan[1] = (self.height() / (2 * self.__zoom)) - (y_min + h / 2)
+        self.update()
+
